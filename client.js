4a3c251ca2e95dd2f657b3872b5ee4fd6728ac2e
Clay Webb <clay_webb@outlook.com>
2022-06-22 19:33:20 -0500

    Assistant was able to create and test a conditional checkpoint function correctly

--- client.js
+++ client.js
@@ -1,16 +1,24 @@
-function firstAddition(numOne, numTwo) {
-    let sum = 0;
-    sum = numOne + numTwo;
-    return sum;
-}
+console.log('** 110 - Conditional **');
 
-function firstAdditionExamples() {
-    console.log('** 100 - ADDITION **');
-    console.log('Example #1');
-    console.log('The answer should be 5:', firstAddition(2, 3));
-    console.log('Example #2');
-    console.log('The answer should be 9:', firstAddition(2, 7));
-}
 
-console.log(firstAdditionExamples());
-// Assistant was able to create and test this function correctly
+
+function conditionalCheck(numOne, numTwo){
+  if (numOne>numTwo) {
+    return "The first number was bigger!"
+  }
+  else if (numTwo>numOne) {
+    return "The second number was bigger!";
+  }
+  else {
+    return "The numbers are the same!";
+  }
+}
+console.log('Example #1:');
+console.log('Is 3 bigger than 2? :', conditionalCheck(3,2));
+
+console.log('Example #2:');
+console.log('Is 7 bigger than 7? :', conditionalCheck(7,7));
+
+console.log('Example #3:');
+console.log('Is 2 bigger than 7? :', conditionalCheck(2,7));
+// Assistant was able to create and test a conditional checkpoint function correctly
