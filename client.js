808089b128f5c5a8a82af78d02004327764081e1
Clay Webb <clay_webb@outlook.com>
2022-06-23 18:05:53 -0500

    Tested the assistants knowledge of React and different aspects within React such as explaining what a component is

--- client.js
+++ client.js
@@ -1,33 +1,24 @@
 // Me
-    // "Create an array called Employee with properties for name, employeeNumber, annualSalary, and reviewRating"
+    // "Explain how the tool React works"
 
 // Assistant
-var employee = [{name: "someone", employeeNumber: 1234, annualSalary: 1234, reviewRating: 5}]
-//--------------------------------------------------------------------------------------------------------------------
-
-// Me
-    // "Convert that array to ES6"
-
-// Assistant
-const employee = [
-    {name: "foo", employeeNumber: 1234, annualSalary: 1234, reviewRating: 5}
-];
-//--------------------------------------------------------------------------------------------------------------------
-
-// Me
-    // "Add two more employee objects to the employees array"
-
-// Assistant
-employee.push(
-    {name: "foo", employeeNumber: 1234, annualSalary: 1234, reviewRating: 5},
-    {name: "bar", employeeNumber: 1235, annualSalary: 1235, reviewRating: 3}
-);
+    // "React is a tool built with JavaScript that makes it easy to create user interfaces. 
+    // It lets you create a flexible UI that updates automatically as data changes."
 //--------------------------------------------------------------------------------------------------------------------
 
 // Me
     // "Explain why this is true, preferably with something I can test."
 
 // Assistant
-    // "employee.push() will add an item to the end of the array. So employee.push({...}); will append an item. 
-    // You can push multiple items at once: employee.push({...}, {...});"
-//-----------------------------------< END OF CONVO >---------------------------------------------------------------------------------
+    // "React lets you update the UI automatically when any data changes. You can see this by creating 
+    // a few React components and updating their state or passed-in properties. You'll see the UI react to the data changes automatically."
+//--------------------------------------------------------------------------------------------------------------------
+
+// Me
+    // "What is a component in React"
+
+// Assistant
+    // "A React component is an isolated section of UI – for example, a button, or a list of items. 
+    // It's implemented as a JavaScript class, and rendered into HTML. This makes it easy to reuse 
+    // components in different places and update them independently."
+//---------------------------------------------< END OF CONVO >-----------------------------------------------------------------------
