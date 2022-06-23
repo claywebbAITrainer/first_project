0d6bf6ed6b4e3116a11e02df77655cb863b03556
Clay Webb <clay_webb@outlook.com>
2022-06-23 18:18:40 -0500

    Testing the assistants understanding of jQuery, specifically the .ready() method

--- client.js
+++ client.js
@@ -1,24 +1,21 @@
+
 // Me
-    // "Explain how the tool React works"
+    // "Specify a function to execute when the DOM is fully loaded using a jQuery method"
 
 // Assistant
-    // "React is a tool built with JavaScript that makes it easy to create user interfaces. 
-    // It lets you create a flexible UI that updates automatically as data changes."
+$(document).ready(/* your function here*/);
 //--------------------------------------------------------------------------------------------------------------------
 
 // Me
     // "Explain why this is true, preferably with something I can test."
 
 // Assistant
-    // "React lets you update the UI automatically when any data changes. You can see this by creating 
-    // a few React components and updating their state or passed-in properties. You'll see the UI react to the data changes automatically."
-//--------------------------------------------------------------------------------------------------------------------
+    // "$(document).ready() fires as soon as the DOM is loaded, 
+    // whereas $(window).load() fires when all external assets (images etc.) are loaded."
+//-------------------------------------------< END OF CONVO >-------------------------------------------------------------------------
 
 // Me
-    // "What is a component in React"
+    // ""
 
 // Assistant
-    // "A React component is an isolated section of UI – for example, a button, or a list of items. 
-    // It's implemented as a JavaScript class, and rendered into HTML. This makes it easy to reuse 
-    // components in different places and update them independently."
-//---------------------------------------------< END OF CONVO >-----------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
