bb6af922d80c968da8f7eded3156f5c1444e644e
Clay Webb <clay_webb@outlook.com>
2022-06-23 16:57:39 -0500

    Tested assistant in creating a function that will filter through an array for a selected genre and return all the titles from that genre in a new array

--- client.js
+++ client.js
@@ -1,39 +1,68 @@
-let myScrabbleTiles = [];
-
-function addObjects(tile, score) {
-    // console.log('In addObjects function');
-    let tileObjects = {};
-    tileObjects.tile = tile;
-    tileObjects.score = score;
-
-    myScrabbleTiles.push(tileObjects);
-    return myScrabbleTiles;
-} // End addObjects function
-
-addObjects('N', 1); // This will add the values into the myScrabbleTiles array
-addObjects('K', 5);
-addObjects('Z', 10);
-addObjects('X', 8);
-addObjects('D', 2);
-addObjects('A', 1);
-addObjects('A', 1);
-
-
-// console.log(myScrabbleTiles);
-
-function testAnswers(array) {
-    let topScoreArray = [];
-    let lowScoreArray = [];
-    for (let i = 0; i < array.length; i++) {
-        if (array[i].score > 5) {
-            topScoreArray.push(array[i]);
-        } else {
-            lowScoreArray.push(array[i]);
-        }
+let bookList = [
+    {
+        title: 'The Race',
+        genre: 'Action'
+    },
+    {
+        title: 'The Journey',
+        genre: 'Adventure'
+    },
+    {
+        title: 'The Elf',
+        genre: 'Fantasy'
+    },
+    {
+        title: 'The Rocket',
+        genre: 'Sci Fi'
+    },
+    {
+        title: 'The Computer',
+        genre: 'Sci Fi'
+    },
+    {
+        title: 'The Unicorn',
+        genre: 'Fantasy'
+    },
+    {
+        title: 'The Person',
+        genre: 'Biography'
+    },
+    {
+        title: 'The Book About Plants',
+        genre: 'Action'
+    },
+    {
+        title: 'The Space Ship',
+        genre: 'SciFi'
+    },
+    {
+        title: 'The Book About Animals',
+        genre: 'Non-Fiction'
+    },
+    {
+        title: 'The Public Speaker',
+        genre: 'Biography'
+    },
+    {
+        title: 'The Burrito',
+        genre: 'Non-Fiction'
+    },
+    {
+        title: 'The Climb',
+        genre: 'Adventure'
     }
-    console.log('Top Scores:', topScoreArray);
-    console.log('Low Scores:', lowScoreArray);
-    let highScore = 29;
-    // return highScore;
+];
+
+var getArrByGenre = function (list, genre) {
+    return list
+        .filter(function (book) {
+            return book.genre === genre
+        })
+        .map(function (book) {
+            return book.title
+        })
 }
-~/.ssh/github-prime
+
+
+
+console.log(getArrByGenre(bookList, 'Fantasy'))
