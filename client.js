a7793362a88d0fffbb2ab8c256cabfceb3b8a81b
Clay Webb <clay_webb@outlook.com>
2022-06-27 16:22:38 -0500

    Testing assistants knowledge of local and global variables

--- client.js
+++ client.js
@@ -1,18 +1,42 @@
-
 // Me
-    // "Specify a function to execute when the DOM is fully loaded using a jQuery method"
+    // "What's the difference between and global and local variable?"
 
 // Assistant
-$(document).ready(/* your function here*/);
+    // "A global variable can be accessed by any other function, module, class, etc. 
+    // A local variable can only be accessed within the function or scope it's defined in."
 //--------------------------------------------------------------------------------------------------------------------
 
 // Me
     // "Explain why this is true, preferably with something I can test."
 
 // Assistant
-    // "$(document).ready() fires as soon as the DOM is loaded, 
-    // whereas $(window).load() fires when all external assets (images etc.) are loaded."
+    // "Global variables can be accessed anywhere in the program. Local variables are only available from within the local scope.
+    // Example:
+    // $global_var = 'foo';
+
+    // function test() {
+    //     $local_var = 'bar';
+
+    //     echo $global_var; // works
+    //     echo $local_var; // works
+    // }
+
+    // echo $local_var: // doesn't work
+
+    // See Variable scope for more information."
 //-------------------------------------------< END OF CONVO >-------------------------------------------------------------------------
+
+// Me
+    // ""
+
+// Assistant
+//--------------------------------------------------------------------------------------------------------------------
+
+// Me
+    // ""
+
+// Assistant
+//--------------------------------------------------------------------------------------------------------------------
 
 // Me
     // ""
